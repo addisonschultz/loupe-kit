aabb2ba5a132e63fe154cac0067a6253c1b93952
Addison <addison@framer.com>
2019-06-18 13:12:56 +0200

    removed shared colors, using loupe color package instead

--- design-system.framerfx/code/canvas.tsx
+++ design-system.framerfx/code/canvas.tsx
@@ -1,18 +1,3 @@
 // WARNING: this file is auto generated, any changes will be lost
-import { createDesignComponent, CanvasStore } from "framer"
+import { createDesignComponent, CanvasStore } from "framer";
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
-
-export const colors = Object.freeze({
-    /** #6B6B6B */
-    "body": "var(--token-1ce8a50f-dd31-4e55-ba15-6c84854d5d0c, rgb(107, 107, 107))",
-    /** #47DA9F */
-    "primary": "var(--token-b87306bc-ed79-4d2c-b669-659752ff1a83, rgb(71, 218, 159))",
-    /** #6F4CE5 */
-    "secondary": "var(--token-93ad7fef-4f3d-45fe-95e7-c77aeec95a11, rgb(111, 76, 229))",
-    /** #D75F78 */
-    "error": "var(--token-a9e0378a-f2cd-43c2-a00f-a9f3effab66f, rgb(215, 95, 120))",
-    /** #FCD978 */
-    "warning": "var(--token-0d7f1d48-c436-4106-ad95-f402ad8cd381, rgb(252, 217, 120))",
-    /** #008892 */
-    "accent": "var(--token-e76b5bf4-d4d4-46f3-a180-edaad58e520b, rgb(0, 136, 146))",
-})
