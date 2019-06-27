ab77d906923cf220ff66e88630ec6db8c93f7515
Addison <addison@framer.com>
2019-06-27 17:43:35 +0200

    added new theme variable fontSize, still need to validate and test

--- design-system/theme.tsx
+++ design-system/theme.tsx
@@ -2,10 +2,13 @@ import { colors } from "../design-system.framerfx/node_modules/@framer/addison.l
 
 const space: string[] = ["0", "4px", "8px", "16px", "32px"];
 
+const fontSize: string[] = ["6px", "12px", "16px", "24px", "32px"];
+
 const font = "-apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif";
 
 export const theme = {
   color: colors,
   font,
+  fontSize,
   space
 };
